e31fcd6dfb910e9ad086b98ade8016011ecbb09c
Slowgun <betman321@gmail.com>
2024-03-15 11:54:37 +0900

    Lv1,Lv2 과제제출

--- 3weekLv2Report/main.swift
+++ 3weekLv2Report/main.swift
@@ -2,7 +2,7 @@
 // Lv 1
 // 변수randomNumber에 Set타입의 Int를 담는곳
 var randomNumber: Set<Int> = []
-var answerNum: Int = 0 //
+
 
 // 랜덤숫자 뽑기
 // randomNumber의 갯수가 3과 다를때 while문 계속반복
@@ -13,6 +13,9 @@ while randomNumber.count != 3 {
     randomNumber.insert(number)
     print(randomNumber)
 }
+var answerNum: Int = Array(randomNumber).reduce(0, { $0 * 10 + $1 })
+print(answerNum)
+
 let game = BaseballGame()
 game.start()
 
@@ -23,21 +26,24 @@ class BaseballGame {
         while true {
                // 1. 유저에게 입력값을 받음
             let userInput = readLine()!.split(separator: " ").map { Int(String($0))! }
-            print(type(of: userInput))
-           // let userInput = Int(readLine()!)!
-             //  print(userInput)  // readLine 첫시도 랜덤숫자가 한번에 안나옴
-           
-    }
-   
+            var userInputInt = userInput.reduce(0, { $0 * 10 + $1 })
+            print(userInputInt)
+            if answerNum == userInputInt {
+                        print("정답입니다!")
+                break
+                    }
+            else {
+                print("올바른 답이 아닙니다.")
+                continue
+            }
+      }
+        
         // 2. 정수로 변환되지 않는 경우 반복문 처음으로 돌아가기
 
         
-    
         }
         // 3. 세자리가 아니거나, 0을 가지거나 특정 숫자가 두번 사용된 경우 반복문 처음으로 돌아가기
-       // func makeAnswer() -> Int {
-           // Set (a, b, c) else {
-              //  print("중복숫자 사용시 종료")
+    
             }
         // 4. 정답과 유저의 입력값을 비교하여 스트라이크/볼을 출력하기
             var strike: Int = 0
